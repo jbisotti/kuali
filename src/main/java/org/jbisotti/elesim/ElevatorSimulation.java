c48b08c203b0803381d3e59507b621d4d7c3e72d
jbisotti <jbisotti@appriss.com>
2018-08-05 17:17:16 -0400

    Add driver class
    This class acts as the driver for the software. It accepts, and
    validates, the number of elevators and floors.

---
+++ src/main/java/org/jbisotti/elesim/ElevatorSimulation.java
@@ -0,0 +1,22 @@
+package org.jbisotti.elesim;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * The driver for the Elevator Simulation.
+ */
+public class ElevatorSimulation {
+
+    private final int numberOfElevators;
+    private final int numberOfFloors;
+
+    public ElevatorSimulation(final int numberOfElevators, final int numberOfFloors) {
+
+        checkArgument(numberOfElevators > 1, "There must be at least 1 elevator");
+        this.numberOfElevators = numberOfElevators;
+
+        checkArgument(numberOfFloors > 1, "There must be at least 1 floor");
+        this.numberOfFloors = numberOfFloors;
+    }
+
+}
---
+++ src/main/java/org/jbisotti/elesim/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The root package for the Elevator Simulator.
+ */
+package org.jbisotti.elesim;
